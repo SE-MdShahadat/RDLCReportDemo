f2d400b3f2fda52f164e386781f00f98d70f1e8e
SE-MdShahadat <se.mdshahadat@gmail.com>
2020-07-22 00:11:40 +0600

    RDLC project created

---
+++ RDLC/CustomContents/JS/StudentReport.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $("#btnGenerate").click(function () {
+        alert("ok");
+    });
+});
+
+var ReportManager = {
+    GenerateReport: function () {
+        var jsonParam = "";
+        var serviceUrl = "../Home/GetStudentReport";
+        ReportManager.GetReport(serviceUrl, jsonParam, onFailed);
+        function onFailed() {
+            alert(error);
+        }
+    },
+    GetReport: function (serviceUrl, jsonParams, errorCallBack) {
+        jQuery.ajax({
+            url: serviceUrl,
+            async: false,
+            type: "POST",
+            data: "{" + jsonParams + "}",
+            contentType: "application/json; charset=utf-8",
+            success: function () {
+                window.open('../Reports/ReportsViewer.aspx', '_newtab');
+            },
+            error: errorCallBack
+        });
+
+    }
+};
+var ReportHelper = {
+
+};
---
+++ RDLC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RDLC.MvcApplication" Language="C#" %>
